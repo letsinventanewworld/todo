7b6e3e17bbe69cce7ad92d7863ca938660d607cc
elakioui <zouhaire.elakioui@gmail.com>
2019-04-29 15:34:26 +0000

    init repo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { WelcomeComponent } from "./welcome/welcome.component";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'todo';
+  title = "todo";
+  message = "welcome to tonasolution";
 }
