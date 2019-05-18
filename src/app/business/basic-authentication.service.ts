1c5d416da3aba093d2101f3d0bf73b821b37ef96
elakioui <zouhaire.elakioui@gmail.com>
2019-05-18 14:46:00 +0000

    added bearer authentication

--- src/app/business/basic-authentication.service.ts
+++ src/app/business/basic-authentication.service.ts
@@ -20,6 +20,20 @@ export class BasicAuthenticationService {
     sessionStorage.removeItem(AUTHENTICATED_USER);
     sessionStorage.removeItem(TOKEN);
   }
+  executeJWTAuthenticationService(username, password) {
+    return this.http
+      .post<any>(`${API_URL}/authenticate`, {
+        username,
+        password
+      })
+      .pipe(
+        map(data => {
+          sessionStorage.setItem(AUTHENTICATED_USER, username);
+          sessionStorage.setItem(TOKEN, `Bearer ${data.token}`);
+          return data;
+        })
+      );
+  }
 
   executeAuthenticationService(username, password) {
     let basicAuthenticationString =
